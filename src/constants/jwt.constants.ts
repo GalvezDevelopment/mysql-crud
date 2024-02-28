ae08303bfda4087a439675db312b9e865a2ff074
Christian Aurelio Galvez Javalera <christiangalvez@Christians-MacBook-Air.local>
2024-02-28 07:02:18 -0700

    Jwt first config

---
+++ src/constants/jwt.constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'uvj4bty_gng@WHC7mnp'
+};
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,16 @@ import { AuthController } from './auth/auth.controller';
 import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/constants/jwt.constants';
 
 @Module({
   controllers: [UserController, AuthController],
   providers: [UserService, AuthService],
   imports: [
-    JwtModule
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      global: true
+    })
   ]
 })
 export class UserModule {}
